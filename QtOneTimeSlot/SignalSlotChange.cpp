43e1efe8fc304e0b38447131631b702ddebda50a
Andreas Fester <Andreas.Fester@gmx.de>
2012-12-03 12:09:36 +0100

    Initial checkin

---
+++ QtOneTimeSlot/SignalSlotChange.cpp
@@ -0,0 +1,40 @@
+/**
+ * This work is licensed under the Creative Commons Attribution 3.0 Unported 
+ * License. To view a copy of this license, visit 
+ * http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative 
+ * Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+
+/* StackOverflow reference:
+ * http://stackoverflow.com/questions/13672866/change-signal-slot-connection-in-a-slot-when-called
+ */
+
+#include "SignalSlotChange.h"
+
+SignalSlotChange::SignalSlotChange(QWidget *parent) : QWidget(parent)
+{
+    button = new QPushButton("Messgage", this);
+    QObject::connect(button, SIGNAL(clicked()), this, SLOT(firstCall()));
+    show();
+}
+
+void SignalSlotChange::firstCall()
+{
+    QMessageBox::information(this, "SignalSlotChange", "First call", QMessageBox::Ok, QMessageBox::NoButton);
+    // Change the signal-slot connection to secondCall()
+    QObject::disconnect(button, SIGNAL(clicked()), this, SLOT(firstCall()));
+    QObject::connect(button, SIGNAL(clicked()), this, SLOT(secondCall()));
+}
+
+void SignalSlotChange::secondCall()
+{
+    QMessageBox::information(this, "SignalSlotChange", "Second call", QMessageBox::Ok, QMessageBox::NoButton);
+}
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    SignalSlotChange ssc;
+    return app.exec();
+}
---
+++ QtOneTimeSlot/SignalSlotChange.h
@@ -0,0 +1,34 @@
+/**
+ * This work is licensed under the Creative Commons Attribution 3.0 Unported 
+ * License. To view a copy of this license, visit 
+ * http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative 
+ * Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+
+/* StackOverflow reference:
+ * http://stackoverflow.com/questions/13672866/change-signal-slot-connection-in-a-slot-when-called
+ */
+
+
+#ifndef SIGNALSLOTCHANGE_H
+#define SIGNALSLOTCHANGE_H
+
+#include <QtGui>
+
+class SignalSlotChange : public QWidget
+{
+Q_OBJECT
+
+public:
+    SignalSlotChange(QWidget *parent = 0);
+
+private slots:
+    void firstCall();
+    void secondCall();
+
+private:
+    QPushButton *button;
+
+};
+
+#endif // SIGNALSLOTCHANGE_H
